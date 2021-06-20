0871601d2f616697ac71df4a1eb8e8c3c29a6d82
kyraymege <egekayra.aymelek11@gmail.com>
2021-06-20 12:47:00 +0300

    update

--- components/header.js
+++ components/header.js
@@ -5,11 +5,11 @@ function header() {
     return (
         <div className="flex flex-col sm:flex-row justify-around m-5 items-center h-auto">
            <a href="/"> <Image className="object-contain animate-pulse h-8 mb-1" src="/logo.png" height={80} width={200} /></a>
-            <div className="flex flex-grow justify-evenly max-w-2xl sm:w-20  ">
-                <Link href="/addcoin" className="font-bold tracking-widest">Add coin</Link>
-             <p className="font-bold tracking-widest">Promote</p>
-             <p className="font-bold tracking-widest">Newsletter</p>
-             <p className="font-bold tracking-widest">Login</p>
+            <div className="flex flex-grow justify-evenly max-w-2xl sm:w-20 items-center ">
+                <Link href="/addcoin"><p className="font-bold tracking-widest cursor-pointer">Add Coin</p></Link>
+                <Link href="/promote"><p className="font-bold tracking-widest cursor-pointer">Promote</p></Link>
+                <Link href="/contact"><p className="font-bold tracking-widest cursor-pointer">Contact</p></Link>
+                <Link href="/signup"><button className="bg-blue-600 hover:bg-blue-300 font-bold w-24 h-10 rounded-md">Sign up</button></Link>
             </div>
            
         </div>
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+function contact() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default contact
