e39efbb8596461f874ec55327dbd3166966a45c9
kyraymege <egekayra.aymelek11@gmail.com>
2021-06-21 02:41:41 +0300

    tables

--- components/table.js
+++ components/table.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { db } from "./firebase";
+import moment from "moment";
 
 function table() {
   const [coins, setCoins] = useState([]);
@@ -18,7 +19,7 @@ function table() {
       )
     );
   }, []);
-  console.log(coins)
+  console.log(coins);
 
   return (
     <div className="flex flex-col max-w-screen-2xl mx-auto p-10">
@@ -30,31 +31,37 @@ function table() {
                 <tr>
                   <th
                     scope="col"
-                    className=" px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className=" font-bold px-12 py-3 text-left text-xs text-gray-500 uppercase tracking-wider"
                   >
                     Coin Name
                   </th>
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className=" px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider"
                   >
-                    Market Cap
-                  </th>
+                    Symbol
+                  </th>                     
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider"
                   >
                     Chain
                   </th>
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider"
+                  >
+                    Market Cap
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider"
                   >
                     Time since launch
                   </th>
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-12 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider"
                   >
                     Votes
                   </th>
@@ -73,44 +80,44 @@ function table() {
                       <div className="flex items-center">
                         <div className="flex-shrink-0 h-10 w-10">
                           <img
-                            className="h-10 w-10 rounded-full"
-                            src={coin.imageUri}
-                            alt=""
+                            className="rounded-full"
+                            src={coin.coin_imageUri}
+                            height={40} width={40}
+                            alt={coin.coin_name}
                           />
                         </div>
                         <div className="ml-4">
                           <div className="text-sm font-medium text-gray-900">
                             {coin.coin_name}
-                          </div>
-                          <div className="text-sm text-gray-500">
-                            {coin.coin_name}
-                          </div>
+                          </div>                          
                         </div>
                       </div>
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">
-                        {coin.coin_name}
-                      </div>
-                      <div className="text-sm text-gray-500">
-                        {coin.coin_name}
-                      </div>
+                    <td className="px-8 py-4 whitespace-nowrap">
+                    <div className="text-sm font-medium text-gray-900">
+                            ${coin.coin_symbol}
+                          </div> 
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
                       <span className="px-2 inline-flex text-xs leading-5  rounded-full bg-green-100 text-green-800 font-bold">
-                        BSC
+                        {coin.coin_chain}
                       </span>
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {coin.coin_name}
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">
+                        $ {coin.coin_marketcap}
+                      </div>                      
+                    </td>                    
+                    <td className="px-12 py-4 whitespace-nowrap text-sm text-gray-500">
+                      <div className="text-sm text-gray-900">
+                        {moment(coin.coin_age,"DD/MM/YYYY").fromNow()}
+                      </div>
+                    
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Vote
-                      </a>
+                      
+                    <button className="bg-white text-blue-600 border-2 border-blue-600  hover:bg-blue-600 hover:text-white hover:border-none font-bold w-24 h-10 rounded-md items-baseline">↑ {coin.coin_votes}</button>
+                    
                     </td>
                   </tr>
                 ))}
