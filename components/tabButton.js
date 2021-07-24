5d7ea1cb459531d3ac2fdffee6587dfeddc9ae51
kyraymege <egekayra.aymelek11@gmail.com>
2021-07-24 23:01:39 +0300

    update

--- components/tabButton.js
+++ components/tabButton.js
@@ -5,13 +5,13 @@ export default function tabButton() {
         <div className="mx-auto container py-6 px-4 flex items-center justify-center w-full">
             <ul className="w-full hidden md:flex items-center mx-6 pb-2 border-b border-white">
                 <li onClick={() => setActiveStatus(1)} className={activeStatus == 1 ? "py-2 px-4 cursor-pointer bg-indigo-100 ease-in duration-150 rounded font-bold  text-2xl  leading-none text-center text-gray-700 " : "py-2 px-4 cursor-pointer  bg-transparent hover:bg-white ease-in duration-150 rounded text-2xl leading-none text-gray-500"}>
-                    All Coins
+                    All Coins 📢
                 </li>
                 <li onClick={() => setActiveStatus(2)} className={activeStatus == 2 ? "py-2 px-4 cursor-pointer bg-indigo-100 ease-in duration-150 rounded ml-24  text-2xl  leading-none font-bold  text-center text-gray-700" : "py-2 px-4 cursor-pointer ml-24 bg-transparent hover:bg-white ease-in duration-150 rounded text-2xl  leading-none text-gray-500"}>
-                    Today's Best
+                    Today's Best 🔥
                 </li>
                 <li onClick={() => setActiveStatus(3)} className={activeStatus == 3 ? "py-2 px-4 cursor-pointer bg-indigo-100 ease-in duration-150 rounded ml-24  text-2xl  leading-none font-bold  text-center text-gray-700" : "py-2 px-4 cursor-pointer ml-24 bg-transparent hover:bg-white ease-in duration-150 rounded text-2xl  leading-none text-gray-500"}>
-                    Added Today
+                    Added Today 🆕
                 </li>
                 
             </ul>
