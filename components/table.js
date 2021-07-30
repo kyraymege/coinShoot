b4f4452829dee649e8562213d328ef19965ebe6e
kyraymege <egekayra.aymelek11@gmail.com>
2021-07-30 18:29:31 +0300

    email signin

--- components/table.js
+++ components/table.js
@@ -109,7 +109,7 @@ function table() {
         </div>
       </div>
 
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-b-none sm:rounded-t-lg ">
             <table className="min-w-full divide-y divide-gray-200 ">
