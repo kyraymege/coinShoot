12347128929a72c16ac1a0598c3c8c63d09b3fcb
kyraymege <egekayra.aymelek11@gmail.com>
2021-07-30 23:28:02 +0300

    google icon

--- pages/signin.js
+++ pages/signin.js
@@ -3,22 +3,21 @@ import { providers, signIn, getSession, csrfToken } from "next-auth/client";
 function signin(csrfToken) {
     return (
         <section className="bg-gray-800 min-h-screen">
-            <div className="mx-auto flex justify-center lg:items-center h-full">
-
-                <div>
-                    <button className="bg-white text-gray-700" onClick={() => signIn("google")}>
-                        Sign in with Google
-                    </button>
-                </div>
+            <div className="pt-16 px-2 flex flex-col items-center justify-center">
+                <h3 className="text-2xl sm:text-3xl xl:text-2xl font-bold leading-tight">Let's sign in your account</h3>
+            </div>
+            <div className="mx-auto flex flex-col justify-center lg:items-center h-full">
+                <button
+                    onClick={() => signIn("google")}
+                    className="mt-12 bg-white transition text-xl font-bold duration-150 ease-in-out focus:outline-none hover:bg-gray-100 rounded text-indigo-700 px-6 py-2">
+                    <img className="w-8 h-8" src="/google-logo.png" alt="google-logo" /></button>
                 <form
                     method="post"
                     action="/api/auth/signin/email"
-                    className="w-full sm:w-4/6 md:w-3/6 lg:w-4/12 xl:w-3/12 text-white py-12 px-2 sm:px-0">
+                    className="w-full sm:w-4/6 md:w-3/6 lg:w-4/12 xl:w-3/12 text-white py-2 px-2 sm:px-0">
                     <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-                    <div className="pt-16 px-2 flex flex-col items-center justify-center">
-                        <h3 className="text-2xl sm:text-3xl xl:text-2xl font-bold leading-tight">Let's sign in your account</h3>
-                    </div>
-                    <div className="mt-12 w-full px-2 sm:px-6">
+
+                    <div className="mt-4 w-full px-2 sm:px-6">
                         <div className="flex flex-col mt-5">
                             <input required
                                 name="email"
